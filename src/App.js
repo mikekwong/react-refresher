df66bdccd9a4c830df59eb2c975a28e32bbc99eb
Mike Wong <wongmdesign@gmail.com>
2019-09-09 22:42:12 -0400

    binding added for click input values

--- src/App.js
+++ src/App.js
@@ -4,13 +4,22 @@ import Person from './Person/Person'
 
 class App extends Component {
   state = {
-    persons: [{ name: 'Max', age: 28 }, { name: 'Emily', age: 20 }],
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Emily', age: 20 },
+      { name: 'John', age: 15 }
+    ],
     otherState: 'some value'
   }
 
-  handleClick = () => {
+  handleClick = newName => {
     this.setState({
-      persons: [{ name: 'Maximillian', age: 28 }, { name: 'Emily', age: 15 }]
+      persons: [
+        { name: newName, age: 28 },
+        { name: 'Emily', age: 15 },
+        { name: 'John', age: 15 }
+      ],
+      otherState: 'some value'
     })
   }
 
@@ -19,16 +28,22 @@ class App extends Component {
       <div className='App'>
         <h1>I'm a react app</h1>
         <p>This is really working</p>
-        <button onClick={this.handleClick}>Switch Name</button>
+        <button onClick={() => this.handleClick('Maximil')}>Switch Name</button>
         <Person
           name={this.state.persons[0].name}
           age={this.state.persons[0].age}
+          // this form of binding is slightly more resoruce efficient
+          click={this.handleClick.bind(this, 'oops')}
         >
           My hobbies: Racing
         </Person>
         <Person
           name={this.state.persons[1].name}
           age={this.state.persons[1].age}
+        />
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
         />
       </div>
     )
