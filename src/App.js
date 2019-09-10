bfcd74941f945313f1d8e3c780f2334eb720ffef
Mike Wong <wongmdesign@gmail.com>
2019-09-10 15:26:02 -0400

    conditional rendering and methods added

--- src/App.js
+++ src/App.js
@@ -7,9 +7,10 @@ class App extends Component {
     persons: [
       { name: 'Max', age: 28 },
       { name: 'Emily', age: 20 },
-      { name: 'John', age: 15 }
+      { name: 'John', age: 15 },
     ],
-    otherState: 'some value'
+    otherState: 'some value',
+    showPersons: false,
   }
 
   handleClick = newName => {
@@ -17,34 +18,72 @@ class App extends Component {
       persons: [
         { name: newName, age: 28 },
         { name: 'Emily', age: 15 },
-        { name: 'John', age: 15 }
+        { name: 'John', age: 15 },
       ],
-      otherState: 'some value'
+      otherState: 'some value',
     })
   }
 
-  render () {
+  onChangeHandler = e => {
+    this.setState({
+      persons: [
+        { name: 'Max', age: 28 },
+        { name: e.target.value, age: 15 },
+        { name: 'John', age: 15 },
+      ],
+    })
+  }
+
+  togglePersonsHandler = () => {
+    this.setState({ showPersons: !this.state.showPersons })
+  }
+
+  render() {
+    const style = {
+      backgroundColor: 'gray',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer',
+    }
+
+    let persons = null
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          <Person
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age}
+            // this form of binding is slightly more resoruce efficient
+            click={this.handleClick.bind(this, 'oops')}
+          >
+            My hobbies: Racing
+          </Person>
+          <Person
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age}
+            changed={this.onChangeHandler}
+          />
+          <Person
+            name={this.state.persons[2].name}
+            age={this.state.persons[2].age}
+          />
+        </div>
+      )
+    }
+
     return (
-      <div className='App'>
+      <div className="App">
         <h1>I'm a react app</h1>
         <p>This is really working</p>
-        <button onClick={() => this.handleClick('Maximil')}>Switch Name</button>
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age}
-          // this form of binding is slightly more resoruce efficient
-          click={this.handleClick.bind(this, 'oops')}
+        <button
+          style={style}
+          onClick={() => this.togglePersonsHandler('Maximil')}
         >
-          My hobbies: Racing
-        </Person>
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-        />
-        <Person
-          name={this.state.persons[2].name}
-          age={this.state.persons[2].age}
-        />
+          Toggle Persons
+        </button>
+        {persons}
       </div>
     )
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,12 +1,14 @@
 import React from 'react'
+import './Person.css'
 
 const Person = props => {
   return (
-    <div>
+    <div className='person'>
       <p onClick={props.click}>
         I'm {props.name} and {props.age} years old
       </p>
       <p>{props.children}</p>
+      <input type='text' onChange={props.changed} value={props.name} />
     </div>
   )
 }
